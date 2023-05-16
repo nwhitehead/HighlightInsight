011fca78ad24658e2022b3e4b11063faae8b4a29
Nathan Whitehead <nwhitehe@gmail.com>
2023-05-16 11:49:15 -0600

    Workong on popup

---
+++ src/PopupApp.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+import { onMounted, ref, reactive } from 'vue';
+
+const configuration = storageBacked('config',
+    reactive({
+        'context': true,
+    })
+);
+
+</script>
+
+<style>
+.btn {
+    @apply font-bold py-2 px-4 rounded;
+}
+.btn-blue {
+    @apply btn bg-blue-500 text-white;
+}
+.btn-blue:hover {
+    @apply bg-blue-700;
+}
+.btn-green {
+    @apply btn bg-green-500 text-white;
+}
+.btn-green:hover {
+    @apply bg-green-700;
+}
+span.note {
+    @apply font-bold;
+}
+</style>
+
+<template>
+    <div class="container min-w-[300px] mx-auto p-4 bg-white">
+        <h1 className="text-2xl font-bold">
+            Highlight→Insight
+        </h1>
+
+        <fieldset class="border border-solid border-stone-300 p-3 mt-4">
+            <legend>Options</legend>
+            <div class="mb-4 select-none">
+                <label>
+                    <input type="checkbox" id="avatar" v-model="configuration.context" class="mr-2 leading-tight" />
+                    Include context of page in request
+                </label>
+            </div>
+        </fieldset>
+    </div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import manifest_chrome from './chrome/manifest.json' assert { type: 'json' } // Node >=17
-import manifest_firefox from './firefox/manifest.json' assert { type: 'json' } // Node >=17
+// import manifest_firefox from './firefox/manifest.json' assert { type: 'json' } // Node >=17
 import vue from '@vitejs/plugin-vue';
 import webExtension from "@samrum/vite-plugin-web-extension";
 
 const is_firefox = (process.env.BROWSER === 'firefox');
-const manifest = is_firefox ? manifest_firefox : manifest_chrome;
+// const manifest = is_firefox ? manifest_firefox : manifest_chrome;
+const manifest = manifest_chrome;
 const outDir = is_firefox ? "dist/firefox/" : "dist/chrome/";
 
 export default defineConfig({
